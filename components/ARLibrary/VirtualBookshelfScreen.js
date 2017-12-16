74c45f8a5f7146a744373d6a1b0f0a8ec737a5a6
Seamus Martin <spmartin823@gmail.com>
2017-12-16 16:10:22 -0500

    virtual books loading.

--- components/ARLibrary/VirtualBookshelfScreen.js
+++ components/ARLibrary/VirtualBookshelfScreen.js
@@ -9,6 +9,25 @@ console.disableYellowBox = true;
 
 export default class VirtualBookshelfScreen extends React.Component {
 
+  
+  bookCreator = async (scene, dimensions, imageUrl) => {
+    // Edit the box dimensions here: 
+    let length = dimensions[0] * .01
+    let height = dimensions[1] * .01
+    let width = dimensions[2] * .01
+
+    const geometry = new THREE.BoxGeometry(length, height, width);
+
+    const texture = await ExpoTHREE.loadAsync(imageUrl); 
+    const material = new THREE.MeshBasicMaterial({ map: texture })
+    
+    const book = new THREE.Mesh(geometry, material);
+    book.position.z = -.4;
+    scene.add(book); 
+  }
+
+
+
   _onGLContextCreate = async (gl) => {
     const width = gl.drawingBufferWidth;
     const height = gl.drawingBufferHeight;
@@ -18,22 +37,19 @@ export default class VirtualBookshelfScreen extends React.Component {
     const scene = new THREE.Scene();
     const camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
     const renderer = ExpoTHREE.createRenderer({ gl });
-    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    renderer.setSize(width, height);
 
     scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
 
-    // Edit the box dimensions here and see changes immediately!
-    const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry);
-    cube.position.z = -0.4;
-    scene.add(cube);
+    // bookCreator takes in the scene, the dimensions (l, h, w), and the 
+    // imageUrl and adds them to the ARScene. 
+    this.bookCreator(scene, [5, 8, 1], 'https://i.imgur.com/mVhn1xd.jpg')
 
     const animate = () => {
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.07;
-      cube.rotation.y += 0.04;
+      // book.rotation.x += 0.07;
+      // book.rotation.y += 0.04;
 
       renderer.render(scene, camera);
       gl.endFrameEXP();
@@ -42,7 +58,6 @@ export default class VirtualBookshelfScreen extends React.Component {
   }
 
   render() {
-    console.log('this is getting run. ')
     return (
       <Expo.GLView
         ref={(ref) => this._glView = ref}
